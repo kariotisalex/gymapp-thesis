39beaf1ecc455bbc3e092d9a4c789db667c4c8b6
akariotis <kariotis.alexander@gmail.com>
2024-04-22 01:18:28 +0300

    add : import dependencies modules

--- gymapp/src/main/java/com/alexkariotis/gymapp/Application.java
+++ gymapp/src/main/java/com/alexkariotis/gymapp/Application.java
@@ -1,9 +1,18 @@
 package com.alexkariotis.gymapp;
 
+import com.alexkariotis.gymapp.domain.entity.EntityConfiguration;
+import com.alexkariotis.gymapp.domain.repository.RepositoryConfiguration;
+import com.alexkariotis.gymapp.service.ServiceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({
+        ServiceConfiguration.class,
+        EntityConfiguration.class,
+        RepositoryConfiguration.class
+})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
