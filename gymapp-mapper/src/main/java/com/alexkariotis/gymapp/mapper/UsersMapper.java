6e37fa35ee88dc0e7a9c03e04530ee9aa1d2f5f5
akariotis <kariotis.alexander@gmail.com>
2024-05-22 22:32:23 +0300

    fix : mapper : fix in birthdate

--- gymapp-mapper/src/main/java/com/alexkariotis/gymapp/mapper/UsersMapper.java
+++ gymapp-mapper/src/main/java/com/alexkariotis/gymapp/mapper/UsersMapper.java
@@ -6,6 +6,10 @@ import com.alexkariotis.gymapp.dto.user.UsersRequestDto;
 import com.alexkariotis.gymapp.dto.user.UsersResponseDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Mapper
 public interface UsersMapper {
@@ -13,7 +17,15 @@ public interface UsersMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
+    @Mapping(source = "birthdate", target = "birthdate", qualifiedByName = "transformation")
     Users toUsers(UsersRequestDto user);
 
     UsersResponseDto toUsersResponseDto(Users user);
+
+    @Named("transformation")
+    public static LocalDate Transformation(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return LocalDate.parse(date, formatter);
+
+    }
 }
